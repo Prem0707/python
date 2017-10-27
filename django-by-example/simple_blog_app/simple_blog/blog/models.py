c8ab540876fd9223e77cbe486aba7280507780d3
emobossemo <408455428@qq.com>
2017-10-27 15:46:30 +0800

    20171027

--- django-by-example/simple_blog_app/simple_blog/blog/models.py
+++ django-by-example/simple_blog_app/simple_blog/blog/models.py
@@ -28,10 +28,10 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        # 'blog:detail': blog 应用下的 name=detail 的函数
-        return reverse('blog:detail', kwargs={'pk': self.pk})
-
-    def increase_views(self):
-        self.views += 1
-        self.save(update_fields=['views'])
+    # def get_absolute_url(self):
+    #     # 'blog:detail': blog 应用下的 name=detail 的函数
+    #     return reverse('blog:detail', kwargs={'pk': self.pk})
+    #
+    # def increase_views(self):
+    #     self.views += 1
+    #     self.save(update_fields=['views'])
--- django-by-example/simple_blog_app/simple_blog/blog/views.py
+++ django-by-example/simple_blog_app/simple_blog/blog/views.py
@@ -1,6 +1,7 @@
 # from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category, Tag
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     # return HttpResponse("欢迎访问我的博客首页！")
@@ -12,8 +13,19 @@ def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     category_list = Category.objects.all()
     tag_list = Tag.objects.all()
+
+    paginator = Paginator(post_list, 2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # 如果用户请求的页码号不是整数，显示第一页
+        posts = paginator.page(1)
+    except EmptyPage:
+        # 如果用户请求的页码号超过了最大页码号，显示最后一页
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/index.html', context={
-        'post_list': post_list,
+        'post_list': posts,
         'category_list': category_list,
         'tag_list': tag_list,
         'title': '博客列表',
